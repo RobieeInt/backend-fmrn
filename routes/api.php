fa728757ff4da320d90d3739d82e90d2160de06d
RobieeInt <74244073+RobieeInt@users.noreply.github.com>
2021-04-26 05:55:46 +0700

    z

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,7 @@ Route::middleware('auth:sanctum')->group(function(){
 
 });
 
+
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
 
